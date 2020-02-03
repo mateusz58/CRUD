ba2fdca968a4a507c0d13a661783984b0a0f778a
mateusz58 <matp321@gmail.com>
2020-02-03 22:20:54 +0100

    Adde task

--- src/main/java/tasks/domain/TaskDto.java
+++ src/main/java/tasks/domain/TaskDto.java
@@ -2,6 +2,7 @@ package tasks.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class TaskDto {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String title;
--- src/main/java/tasks/repository/TaskRepository.java
+++ src/main/java/tasks/repository/TaskRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tasks.domain.TaskDto;
 
 public interface TaskRepository extends JpaRepository<TaskDto, Long> {
-
 }
